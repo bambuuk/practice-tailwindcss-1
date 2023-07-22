4917a67de651b9aca956ccebb81eb1adca462498
Ivan <vanyok.korobka@ukr.net>
2023-07-22 23:36:49 +0300

    added styles to nav icons and script to public.html

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,9 +19,9 @@ const Navbar = () => {
         <li className="m-2 p-2 cursor-pointer hover:text-[#00df9a] transition-colors">Contact</li>
       </ul>
       <div onClick={handleNav} className='block md:hidden'>
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        {nav ? <AiOutlineClose size={20} className='fixed right-4 top-7' color={'#00df9a'} /> : <AiOutlineMenu size={20} className='fixed right-4 top-7' color={'#00df9a'} />}
       </div>
-      <div className={!nav ? 'fixed left-[-100%] ease-in-out duration-500' : 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'}>
+      <div className={!nav ? 'z-10 fixed left-[-100%] ease-in-out duration-500' : 'z-10 fixed left-0 top-0 w-[60%] h-full border-r-gray-500 border-r-4 bg-[#000300] ease-in-out duration-500'}>
         <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">React</h1>
         <ul className='p-4 uppercase'>
           <li className="p-4 border-b border-gray-600">Home</li>
